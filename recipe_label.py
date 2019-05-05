491f8271c51d8f0f816e9e58d682c09eedd63b5f
jinhalim <1004halim@naver.com>
2019-05-05 15:28:20 +0900

    레시피 정리 ㅜㅠㅜㅠㅜ

--- recipe_label.py
+++ recipe_label.py
@@ -1,9 +1,22 @@
 import os
+import json
 
+seach = '&&'
+seach1 = '**'
+def textlist(f,newtext):
+    list1 = []
+    for line in f.readlines():
+        line1 = line.split('\n')
+        # # print(f.readline())
+        if(line1[0] in seach):
+            # print(list1)
+            newtext.writelines(list1)
+            # newtext.writelines('\n')
+            list1 = []
+            list1.append(line1[0])
+        else:
+            list1.append(line1[0])
 
-def textlist(f):
-    for line in f:
-        print(f.readline())
 
 i = 0
 for root, subdirs, files in os.walk('./please_v2'):
@@ -11,8 +24,8 @@ for root, subdirs, files in os.walk('./please_v2'):
         with open("./please_v2/"+file, 'r') as f:
             i += 1
             s = str(i)
-            textlist(f)
             newtext = open("./please_v2/"+s+".txt", 'w')
+            textlist(f,newtext)
             
 
 
