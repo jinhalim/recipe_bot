0784fcb346b20ae938112885c45847b8dcdfcf65
jinhalim <1004halim@naver.com>
2019-05-09 16:15:36 +0900

    재료 추가 했고, big_category table완성 ㄷㄷㄷㄷㄷ

--- insertdb.py
+++ insertdb.py
@@ -5,10 +5,13 @@ myJson = {}
 tempRecipe = open("C:/Users/jinhalim/Desktop/recipe_bot/dessert.txt","r")
 
 i = 0
+count = 900
 for line in tempRecipe.readlines():
     subline = line.split('\n')
-    myJson[i] = subline[0].split('\t')
-    print("insert into big_category values ("+myJson[i][0]+", '"+myJson[i][1]+"') ;")
+    myJson[i] = subline[0]
+    count += 1
+    count_1 = str(count)
+    print("insert into big_category values ("+ count_1 +", '"+subline[0]+"') ;")
     i += 1
 
 # print(myJson)
--- label.py
+++ label.py
@@ -1,16 +1,5 @@
 import json
-
-myJson = {}
-
-tempRecipe = open("C:/Users/jinhalim/Desktop/recipe_bot/please_recipe_text/국물.txt","r")
-newRecipe = open("./test.txt","w")
-
-i = 0
-for line in tempRecipe.readlines():
-    subline = line.split('\n')
-    myJson[i] = subline[0]
-    i += 1
-
+import os
 def check_list(li1):
     j = 0
     for j in li1:
@@ -52,57 +41,65 @@ class Recipe :
         else:
             print("no")
 
+myJson = {}
 
-i = 0
-bt1 = False # 제목
-bt2 = False # 재료
-bt3 = False # 육수
-bt4 = False # 양념
-name_count = 0
-all_recipe = {}
-r = Recipe() 
-for i in range(len(myJson)):
-    if(myJson[i] == "[제목]"):
-        n=0
-        # newRecipe.writelines( [%n for n in r.getall()])
-        for n in r.getall():
-            newRecipe.writelines(n)
-        newRecipe.writelines("\n")
+# tempRecipe = open("C:/Users/jinhalim/Desktop/recipe_bot/food/국물.txt","r")
+# newRecipe = open("./test.txt","w")
+k=0
+for root, subdirs, files in os.walk('./food'):
+    for file in files:
+        with open("./food/"+file, 'r') as tempRecipe:
+            k += 1
+            s = str(k)
+            newRecipe = open("./food/"+s+".txt", 'w')
+            i = 0
+            for line in tempRecipe.readlines():
+                subline = line.split('\n')
+                myJson[i] = subline[0]
+                i += 1
+            i = 0
+            bt1 = False # 제목
+            bt2 = False # 재료
+            bt3 = False # 육수
+            bt4 = False # 양념
+            name_count = 0
+            all_recipe = {}
+            r = Recipe() 
+            for i in range(len(myJson)):
+                if(myJson[i] == "[제목]"):
+                    n=0
+                    # newRecipe.writelines( [%n for n in r.getall()])
+                    for n in r.getall():
+                        newRecipe.writelines(n)
+                    newRecipe.writelines("\n")
 
-        print(r.getall())
-        r = Recipe() 
-        name_count += 1
-        bt1 = True
-        bt2 = False
-        bt3 = False
-        bt4 = False
-        # print(myJson[i])
-    elif(myJson[i] == "[재료]"):
-        bt1 = False
-        bt2 = True
-        bt3 = False
-        bt4 = False
-        # print(myJson[i])
-    elif(myJson[i] == "[육수]"):
-        bt1 = False
-        bt2 = False
-        bt3 = True
-        bt4 = False
-        # print(myJson[i])
-    elif(myJson[i] == "[양념]"):
-        bt1 = False
-        bt2 = False
-        bt3 = False
-        bt4 = True
-        # print(myJson[i])
-    else:
-        r.insert_list(myJson[i],bt1,bt2,bt3,bt4)
-        
-        
-# newRecipe.writelines(r)
-
-# r.print_all()
-newRecipe.close()
-tempRecipe.close()
-
-
+                    print(r.getall())
+                    r = Recipe() 
+                    name_count += 1
+                    bt1 = True
+                    bt2 = False
+                    bt3 = False
+                    bt4 = False
+                    # print(myJson[i])
+                elif(myJson[i] == "[재료]"):
+                    bt1 = False
+                    bt2 = True
+                    bt3 = False
+                    bt4 = False
+                    # print(myJson[i])
+                elif(myJson[i] == "[육수]"):
+                    bt1 = False
+                    bt2 = False
+                    bt3 = True
+                    bt4 = False
+                    # print(myJson[i])
+                elif(myJson[i] == "[양념]"):
+                    bt1 = False
+                    bt2 = False
+                    bt3 = False
+                    bt4 = True
+                    # print(myJson[i])
+                else:
+                    r.insert_list(myJson[i],bt1,bt2,bt3,bt4)
+            newRecipe.close()
+            tempRecipe.close()
