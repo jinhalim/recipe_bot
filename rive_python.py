b80175b1b6f2a12a610c2bfd6ba33dbd53f1fd32
jinhalim <1004halim@naver.com>
2019-05-17 17:57:34 +0900

    db에 있는 거 불러와짐

--- rive_python.py
+++ rive_python.py
@@ -10,16 +10,7 @@ rs.unicode_punctuation = re.compile(r'[.,!?;:]')
 rs.load_directory("./rivescript")
 rs.sort_replies()
 
-print("""This is a bare minimal example for how to write your own RiveScript bot!
-
-For a more full-fledged example, try running: `python rivescript brain`
-This will run the built-in Interactive Mode of the RiveScript library. It has
-some more advanced features like supporting JSON for communication with the
-bot. See `python rivescript --help` for more info.
-
-example3.py is just a simple script that loads the RiveScript documents from
-the 'brain/' folder, and lets you chat with the bot.
-
+print("""
 Type /quit when you're done to exit this example.
 """)
 print("Bot> 무엇이 필요하신가요?")
@@ -29,7 +20,4 @@ while True:
         quit()
     reply = rs.reply("localuser", msg)
     print("Bot>", reply)
-
-# vim:expandtab
-
 
