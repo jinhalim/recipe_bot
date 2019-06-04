4052158dfdc44fe4834a67989dc0d53f0ce59bf5
jinhalim <1004halim@naver.com>
2019-06-05 02:52:54 +0900

    1,2추가

--- rive_python.py
+++ rive_python.py
@@ -2,7 +2,7 @@
 
 # Python 3 example
 
-from rivescript import RiveScript
+from rivescript import RiveScript 
 import re
 rs = RiveScript(utf8=True)
 # rs.set_handler("python", None)
@@ -14,6 +14,7 @@ print("""
 Type /quit when you're done to exit this example.
 """)
 print("Bot> 무엇이 필요하신가요?")
+print("1: 요리 검색\n2: 재료 검색\n3: 기타")
 while True:
     msg = input("You> ")
     if msg == '/quit':
